24ab6b8d3e12fd0b7926ebfac046e21291babe01
KimDelbarre <kim.delbarre@gmail.com>
2020-07-16 12:28:59 +0200

    add another working script

--- Series-of-73-js-exercises/03-colors/02-change-bcg-two/script.js
+++ Series-of-73-js-exercises/03-colors/02-change-bcg-two/script.js
@@ -9,8 +9,12 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
-
+(function () {
+  document.getElementById("run").addEventListener("click", function () {
+    let x = document.querySelector(".field input").value;
+    x = x.toString();
+    console.log(x);
+    document.body.style.backgroundColor = x;
+  });
+  // your code here
 })();
