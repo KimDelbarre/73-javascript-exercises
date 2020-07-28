31c2306f3ed2a0873c2651f16e031d1a7ecaabe9
KimDelbarre <kim.delbarre@gmail.com>
2020-07-28 11:12:36 +0200

    add some kind of text wave

--- Series-of-73-js-exercises/09-misc/01-waving-text/script.js
+++ Series-of-73-js-exercises/09-misc/01-waving-text/script.js
@@ -10,9 +10,57 @@
 // You will have time to focus on it later.
 
 (function () {
-  let mySpan = document.createElement("span");
+  let txt = document.getElementById("target").innerHTML;
+  let size = 4;
+  let result;
+  for (i = 0; i < txt.length; i++) {
+    result += txt
+      .charAt(i)
+      .fontsize(Math.round(5 + 15 * (i / 10 - Math.floor(i / 10))));
+  }
+  console.log(result);
+  document.getElementById("target").innerHTML = result;
 
-  mySpan.innerHTML = document.getElementById("target").style.fontSize =
-   40 px ;
-  console.log(mySpan);
+  /*
+  
+  result =
+    txt.charAt(0).fontsize(10) +
+    txt.charAt(1).fontsize(8) +
+    txt.charAt(2).fontsize(6) +
+    txt.charAt(3).fontsize(4) +
+    txt.charAt(4).fontsize(6) +
+    txt.charAt(5).fontsize(8);
+  document.getElementById("target").innerHTML = result;
+
+
+  for (i = 0; i < txt.length; i++) {
+    console.log(txt.charAt(i));
+
+    result = txt.charAt(i).fontsize(size++);
+  }
+
+  txt.forEach(element => {
+
+  }); (i = 0; i < txt.length; i++) {
+    result = element.fontsize(x++);
+  }
+
+  
+
+    // let result = txt.fontsize(size);
+
+    let result = "";
+  txt.split("").forEach(function(letter) {
+    letter.fontsize(x++);
+  });
+  console.log(result);
+  
+  
+  function wave {
+txt.forEach(element => {
+
+  
+
+
+  */
 })();
